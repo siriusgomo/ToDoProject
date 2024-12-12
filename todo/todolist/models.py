3a835f34da91ce50f985dc80cc7f52381eb4d9ac
siriusgomo <siriusgomo04@gmail.com>
2024-12-12 14:02:02 +0300

    Create Event model, and superuser, but server error.

--- todo/todolist/models.py
+++ todo/todolist/models.py
@@ -1,3 +1,16 @@
-from django.db import models
 
-# Create your models here.
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    context = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
